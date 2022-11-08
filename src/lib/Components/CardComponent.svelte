4b10a88d9c20d1bab6ada6da1c75b8372f9df9fd
Luis C <luiscruzv99@gmail.com>
2022-11-08 12:30:52 +0100

    Expanded About Me section

--- src/lib/Components/CardComponent.svelte
+++ src/lib/Components/CardComponent.svelte
@@ -1,20 +1,21 @@
-<script lang='ts'>
-    export let titulo = ""
-    export let desc = ""
-    export let imgSrc = ""
+<script lang="ts">
+	export let titulo = '';
+	export let desc = '';
+	export let imgSrc = '';
+	export let imgLink = '';
 </script>
 
-
 <div class="flex flex-col lg:flex-row rounded-xl bg-cpink my-10 drop-shadow-lg">
-    <div class="flex p-5 items-center m-auto w-3/4 md:w-1/3">
-        <img src={imgSrc} alt="" class="rounded-full"/>
-    </div>
-    <div class="block p-8 w-full">
-        <h2 class="text-left">{titulo}</h2>
-        <div class="font-dosis text-xl">
-            {#each desc.split('\n') as line }
-                <p class="p-2 md:text-justify">{line}</p>
-            {/each}
-        </div>
-    </div>
+	<div class="flex p-5 items-center m-auto w-3/4 md:w-1/3">
+		<a href={imgLink} target="_blank"><img src={imgSrc} alt="" class="rounded-full" /> </a>
+	</div>
+
+	<div class="block p-8 w-full">
+		<h2 class="text-left">{titulo}</h2>
+		<div class="font-dosis text-xl">
+			{#each desc.split('\n') as line}
+				<p class="p-2 md:text-justify">{line}</p>
+			{/each}
+		</div>
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,8 @@
 </script>
 
 <div class="mx-5 md:mx-20 lg:mx-40 xl:mx-80">
-	<CardComponent titulo={'About Me'} desc={Luis} imgSrc={face} />
+	<CardComponent titulo={'About Me'} desc={Luis} imgSrc={face} imgLink="https://github.com/luiscruzv99" />
+
 	<hr />
 </div>
 
