269a1fdc7dc3f047458d07d118cded7ee52c7bf9
Luis Cruz <luiscruzv99@gmail.com>
2023-05-05 21:09:14 +0200

    Reworked header

--- src/lib/Components/Header.svelte
+++ src/lib/Components/Header.svelte
@@ -1,12 +1,13 @@
 <script>
-    import { fly } from 'svelte/transition'
-    import ham from '$lib/assets/ham.svg'
+	import { fly } from 'svelte/transition';
+	import ham from '$lib/assets/ham.svg';
+	import logo from '$lib/assets/logo.png';
 
-    let links =[
-        {name: "Home", link: "."},
-        {name: "Blog", link: "./blog"},
-        {name: "Projects", link: "./projects"}
-    ]
+	let links = [
+		{ name: 'Home', link: '/' },
+		{ name: 'Blog', link: '/blog' },
+		{ name: 'Projects', link: '/projects' }
+	];
 
     let open = false;
 
@@ -17,26 +18,31 @@
 </script>
 
 <header>
-    <div class="flex">
-        <h2>LuisC.dev</h2>
-    </div>
-    <div class="xl-nav">
-        {#each links as route}
-            <a href={route.link} >
-                <h3 class=" hover:scale-125 hover:text-antigerine transition ease-in-out duration-200">{route.name}</h3>
-            </a>
-        {/each}
-    </div>
-    <div class="mobile-nav z-20" on:click="{showMenu}">
-        <img src={ ham } alt="">
-        {#if open}
-            <div class="menu-display" transition:fly={{ duration: 200}}>
-                {#each links as route, i }
-                    <a href="{route.link}" in:fly={{ delay: 50 * i }}>
-                        <h3 class="menu-item">{route.name}</h3>
-                    </a>
-                {/each}
-            </div>
-        {/if}
-    </div>
+	<div class="flex">
+        <a href="/" class="hover:scale-110 hover:font-bold transition ease-in-out duration-200 flex">
+		<img src={logo} class=" h-16 p-2" alt=""/>
+		<h2>LuisC.dev</h2>
+        </a>
+	</div>
+	<div class="xl-nav">
+		{#each links as route}
+			<a href={route.link}>
+				<h3 class="px-4 hover:scale-150 hover:font-bold transition ease-in-out duration-200">
+					{route.name}
+				</h3>
+			</a>
+		{/each}
+	</div>
+	<div class="mobile-nav z-20" on:click={showMenu}>
+		<img src={ham} alt="" />
+		{#if open}
+			<div class="menu-display" transition:fly={{ duration: 200 }}>
+				{#each links as route, i}
+					<a href={route.link} in:fly={{ delay: 50 * i }}>
+						<h3 class="menu-item">{route.name}</h3>
+					</a>
+				{/each}
+			</div>
+		{/if}
+	</div>
 </header>
